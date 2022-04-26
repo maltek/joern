345b7b3182ecabf45298e04d75cc12ea3e22c470
Max Leuthäuser <max.leuthaeuser@gmail.com>
2022-04-26 09:34:05 +0200

    Support for more literals

--- joern-cli/frontends/jssrc2cpg/src/main/scala/io/joern/jssrc2cpg/astcreation/AstForPrimitivesCreator.scala
+++ joern-cli/frontends/jssrc2cpg/src/main/scala/io/joern/jssrc2cpg/astcreation/AstForPrimitivesCreator.scala
@@ -15,6 +15,9 @@ trait AstForPrimitivesCreator {
     Ast(identNode)
   }
 
+  protected def astForNullLiteral(nullLiteral: BabelNodeInfo): Ast =
+    Ast(createLiteralNode(nullLiteral.code, Some(Defines.NULL.label), nullLiteral.lineNumber, nullLiteral.columnNumber))
+
   protected def astForStringLiteral(stringLiteral: BabelNodeInfo): Ast =
     Ast(
       createLiteralNode(
@@ -25,6 +28,36 @@ trait AstForPrimitivesCreator {
       )
     )
 
+  protected def astForRegExpLiteral(regExpLiteral: BabelNodeInfo): Ast =
+    Ast(
+      createLiteralNode(
+        regExpLiteral.code,
+        Some(Defines.STRING.label),
+        regExpLiteral.lineNumber,
+        regExpLiteral.columnNumber
+      )
+    )
+
+  protected def astForRegexLiteral(regexLiteral: BabelNodeInfo): Ast =
+    Ast(
+      createLiteralNode(
+        regexLiteral.code,
+        Some(Defines.STRING.label),
+        regexLiteral.lineNumber,
+        regexLiteral.columnNumber
+      )
+    )
+
+  protected def astForNumberLiteral(numberLiteral: BabelNodeInfo): Ast =
+    Ast(
+      createLiteralNode(
+        numberLiteral.code,
+        Some(Defines.NUMBER.label),
+        numberLiteral.lineNumber,
+        numberLiteral.columnNumber
+      )
+    )
+
   protected def astForNumericLiteral(numericLiteral: BabelNodeInfo): Ast =
     Ast(
       createLiteralNode(
@@ -32,6 +65,36 @@ trait AstForPrimitivesCreator {
         Some(Defines.NUMBER.label),
         numericLiteral.lineNumber,
         numericLiteral.columnNumber
+      )
+    )
+
+  protected def astForDecimalLiteral(decimalLiteral: BabelNodeInfo): Ast =
+    Ast(
+      createLiteralNode(
+        decimalLiteral.code,
+        Some(Defines.NUMBER.label),
+        decimalLiteral.lineNumber,
+        decimalLiteral.columnNumber
+      )
+    )
+
+  protected def astForBigIntLiteral(bigIntLiteral: BabelNodeInfo): Ast =
+    Ast(
+      createLiteralNode(
+        bigIntLiteral.code,
+        Some(Defines.NUMBER.label),
+        bigIntLiteral.lineNumber,
+        bigIntLiteral.columnNumber
+      )
+    )
+
+  protected def astForBooleanLiteral(booleanLiteral: BabelNodeInfo): Ast =
+    Ast(
+      createLiteralNode(
+        booleanLiteral.code,
+        Some(Defines.BOOLEAN.label),
+        booleanLiteral.lineNumber,
+        booleanLiteral.columnNumber
       )
     )
 
